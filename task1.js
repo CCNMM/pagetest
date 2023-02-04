1a2428b22f3d93162fab42af80e14dfdcb415e06
CCNMM <124461169+CCNMM@users.noreply.github.com>
2023-02-04 21:04:45 +0000

    add timer function

--- task1.js
+++ task1.js
@@ -1,6 +1,48 @@
-const countdown = (min) => {
-  const minutes = min;
-  const seconds = 60;
-};
+function countdown(min, elem) {
+  let minutes = min;
+  let seconds = 0;
+  let timer = elem;
+  let start = false;
 
-countdown(2);
+  function updateElem() {
+    if (seconds < 10) {
+      timer.innerText = minutes + ":0" + seconds;
+    } else {
+      timer.innerText = minutes + ":" + seconds;
+    }
+  }
+
+  function counter() {
+    // Top interval if minutes and second = 0
+    if (minutes <= 0 && seconds <= 0) {
+      updateElem();
+      clearInterval(interval);
+    } else {
+      //   Initial timer start
+      if (start === false) {
+        updateElem();
+        start = true;
+      } else {
+        if (seconds === 0) {
+          // if seconds reach 0 re-set to 60 and minus 1 from minutes
+          seconds = 60;
+          seconds--;
+          minutes--;
+          updateElem();
+        } else {
+          // minus seconds per second
+          seconds--;
+          updateElem();
+        }
+      }
+    }
+  }
+
+  const interval = setInterval(counter, 1000); // Interval every second
+}
+
+window.onload = function () {
+  const minutes = 2;
+  const timer = document.querySelector(".countdown .timer");
+  countdown(minutes, timer); // Function run
+};
